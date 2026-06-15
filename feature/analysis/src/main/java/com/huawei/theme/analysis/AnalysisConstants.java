5e0de7ae8f799a9fb8cbb236676b18092414084c
araragi-koyomin <3099146950@qq.com>
2026-06-15 15:19:48 +0800

    chore: phase1.1 project skeleton - Gradle init, intellij plugin config, plugin.xml registration

---
+++ feature/analysis/src/main/java/com/huawei/theme/analysis/AnalysisConstants.java
@@ -0,0 +1,8 @@
+package com.huawei.theme.analysis;
+
+public final class AnalysisConstants {
+    public static final String PLUGIN_ID = "com.huawei.theme.analysis";
+
+    private AnalysisConstants() {
+    }
+}
