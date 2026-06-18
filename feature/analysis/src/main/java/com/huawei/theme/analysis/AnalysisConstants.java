41b9ccfa571190184835e8c0f51fa6ffbfb8548b
araragi-koyomin <144544985+araragi-koyomin@users.noreply.github.com>
2026-06-18 16:25:59 +0800

    chore: remove analysis java sources/tests, docs/report, Development-Plan, and raw_markdown files

--- feature/analysis/src/main/java/com/huawei/theme/analysis/AnalysisConstants.java
+++
@@ -1,8 +0,0 @@
-package com.huawei.theme.analysis;
-
-public final class AnalysisConstants {
-    public static final String PLUGIN_ID = "com.huawei.theme.analysis";
-
-    private AnalysisConstants() {
-    }
-}
